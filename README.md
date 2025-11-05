6776ab6e26059e4b1f744bb4618822382b89bbdf
Tan Yong Jie <Tan_Yong_Jie@outlook.com>
2025-11-06 01:04:23 +0800

    Different .world [Working]

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ ros2 launch turtlebot3_gazebo empty_world.launch.py
 
 # Spawn World
 ros2 launch maze_explorer maze_world.launch.py world_name:=tb3_maze.world x_pose:=0.0  y_pose:=-0.3
+ros2 launch maze_explorer maze_world.launch.py world_name:=maze_walls_full.world x_pose:=1.5 y_pose:=1.3
 
 # ============ TERMINAL 2 ============ (new terminal)
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/home/yongjie/ros2_assign_ws/install/maze_explorer/share/maze_explorer/models
@@ -18,4 +19,4 @@ colcon build --packages-select maze_explorer
 ros2 launch maze_explorer navigation.launch.py use_sim_time:=true
 
 # ============ TERMINAL 3 ============ (new terminal)
-ros2 run maze_explorer nav_goal_sender 
+ros2 run maze_explorer nav_goal_sender
--- launch/maze_world.launch.py
+++ launch/maze_world.launch.py
@@ -5,7 +5,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 def generate_launch_description():
     # Package directories
@@ -15,15 +15,16 @@ def generate_launch_description():
 
     # Launch configurations
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+    world_name = LaunchConfiguration('world_name', default='tb3_maze.world')
     x_pose = LaunchConfiguration('x_pose', default='0.0')
     y_pose = LaunchConfiguration('y_pose', default='0.0')
 
-    # World file path
-    world = os.path.join(
+    # World file path - using PathJoinSubstitution
+    world = PathJoinSubstitution([
         maze_explorer_dir,
         'worlds',
-        'tb3_maze.world'
-    )
+        world_name
+    ])
 
     # Gazebo server
     gzserver_cmd = IncludeLaunchDescription(
